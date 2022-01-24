9200e704de31f81e31611a541a969b4551eb8daf
wahiddwi <wahiddwi7@gmail.com>
2022-01-24 17:42:39 +0700

    workspace

---
+++ src/pages/landingPages.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import modulName from 'elements/Button';
+
+export default class landingPages extends Component {
+  render() {
+    return <div>
+        
+    </div>;
+  }
+}
