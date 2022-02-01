744eb760433f2ab843727e75a5df84eb4feaa6e1
wahiddwi <wahiddwi7@gmail.com>
2022-02-01 21:14:05 +0700

    grid mostpicked

--- src/pages/landingPages.js
+++ src/pages/landingPages.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from 'parts/header';
 import Hero from 'parts/Hero';
+import MostPicked from 'parts/MostPicked';
 
 import landingPage from 'json/landingPage.json';
 
@@ -11,6 +12,7 @@ export default class LandingPages extends Component {
       <>
         <Header {...this.props}></Header>
         <Hero data={landingPage.hero} />
+        <MostPicked data={landingPage.mostPicked} />
       </>
     );
   }
