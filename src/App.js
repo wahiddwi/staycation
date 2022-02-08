c86c37da58199ee4a19ba9390245bd6e89343a63
wahiddwi <wahiddwi7@gmail.com>
2022-02-08 20:30:42 +0700

    component input date & input number

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'; //sortcat => 
 
 import "assets/scss/style.scss";
 import LandingPages from 'pages/LandingPages';
+import Example from 'pages/Example';
 
 const history = createBrowserHistory({
   basename: process.env.PUBLIC_URL,
@@ -12,7 +13,8 @@ const history = createBrowserHistory({
 function App() {
   return <div className="App">
       <Router history={history}>
-        <Route path="/" component={LandingPages}></Route>
+        <Route exact path="/" component={LandingPages}></Route>
+        <Route path="/example" component={Example}></Route>
       </Router>
     </div>;
 }
