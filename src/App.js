faf6d8d354e964ca7c69cfb6e90b04f262ccc3ca
wahiddwi <wahiddwi7@gmail.com>
2022-04-07 23:14:44 +0700

    complete image & routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React from "react";
 import { createBrowserHistory } from "history";
-import { BrowserRouter as Router, Route } from 'react-router-dom'; //sortcat => imrr
+import {
+	BrowserRouter as Router,
+	Route,
+} from "react-router-dom"; //sortcat => imrr
 
 import "assets/scss/style.scss";
-import LandingPages from 'pages/LandingPages';
-import DetailsPage from 'pages/DetailsPage';
-import Checkout from 'pages/Checkout';
-import Example from 'pages/Example';
+import LandingPages from "pages/LandingPages";
+import DetailsPage from "pages/DetailsPage";
+import Checkout from "pages/Checkout";
+import Example from "pages/Example";
 
 const history = createBrowserHistory({
-  basename: process.env.PUBLIC_URL,
+	basename: process.env.PUBLIC_URL,
 });
 
 function App() {
-  return <div className="App">
-      <Router history={history}>
-        <Route exact path="/" component={LandingPages} />
-        <Route exact path="/properties/:id" component={DetailsPage} />
-        <Route path="/checkout" component={Checkout} />
-        <Route path="/example" component={Example} />
-      </Router>
-    </div>;
+	return (
+		<div className="App">
+			<Router history={history}>
+				<Route
+					exact
+					path="/"
+					component={LandingPages}
+				/>
+				<Route
+					exact
+					path="/properties/:id"
+					component={DetailsPage}
+				/>
+				<Route
+					path="/checkout"
+					component={Checkout}
+				/>
+				<Route path="/example" component={Example} />
+			</Router>
+		</div>
+	);
 }
 
 export default App;
